73b40aabbe9fee304861fdcb986e19fa4944af95
Mateus Mota <mateus@marketmedia.com.br>
2022-01-15 19:23:38 -0300

    add create, update and delete tasks

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,38 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
+  function getRandomInt(min: number, max: number) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+
+      return Math.floor(Math.random() * (max - min)) + min;
+  }
+
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    const id = getRandomInt(0, 999);
+
+    console.log(newTaskTitle)
+
+    if(newTaskTitle !== '') {
+      setTasks([ ...tasks, { id: id, title: newTaskTitle, isComplete: false }])
+
+      setNewTaskTitle('');
+    }
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const editedTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
+    
+    setTasks(editedTasks)
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task => task.id !== id);
+
+    setTasks([...filteredTasks])
   }
 
   return (
